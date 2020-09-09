2fbe9b655e69cbeec0c4b5f6461a846e1fed9fb3
TAIKI IKEDA <taiki_ikeda@relationsgroup.co.jp>
2020-09-09 18:42:13 +0900

    Modify color and button stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ThemeProvider } from '../src/themes/context'
-import { lightTheme } from '../src/themes'
+import { lightTheme, darkTheme } from '../src/themes'
 
 export const decorators = [
   (storyFn) => <ThemeProvider theme={lightTheme}>{storyFn()}</ThemeProvider>,
--- src/foundations/colors/light/index.ts
+++ src/foundations/colors/light/index.ts
@@ -6,8 +6,8 @@ export const lightColors: {
 } = {
   body: palette.white,
   background: palette.white,
-  'background:hover': palette['gray-400'],
-  'background:active': palette['gray-500'],
+  'background:hover': palette['gray-100'],
+  'background:active': palette['gray-300'],
   border: palette['gray-500'],
   outline: palette['gray-800'],
 }
